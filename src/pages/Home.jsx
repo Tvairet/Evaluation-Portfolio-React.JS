8579723908c06221cf9a936af412e0ac062978c6
Tristan <tristan.vairet@gmail.com>
2026-04-09 11:30:36 +0200

    creation des pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () =>{
+    return (
+        <main className="container">
+            <h1>Bonjour, je suis John Doe</h1>
+            <h2>Développeur web full stack</h2>
+        </main>
+    );
+};
+
+export default Home;
