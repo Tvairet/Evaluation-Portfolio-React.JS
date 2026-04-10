dc5141f9bbe9b469de1505014f0ed3fab8ef96e1
Tristan <tristan.vairet@gmail.com>
2026-04-10 09:53:58 +0200

    creation header

---
+++ src/pages/Mentions.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Mentions = () => {
+    return (
+        <div className="container">
+            <h1>
+                Mentions légales
+            </h1>
+        </div>
+    );
+};
+
+export default Mentions;
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Portfolio = () => {
+    return (
+        <main className="container">
+        <h1>Portfolio</h1>
+        </main>
+    );
+};
+
+export default Portfolio;
